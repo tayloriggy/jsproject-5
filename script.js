992d6c8cd0d87945ea2e83943eea4abdf04d9a85
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2021-10-25 20:37:23 -0500

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+Project 5: Build a clock using JavaScript.
+
+Requirements:
+-Create a Github repository for this project. 
+-Display the hours, minutes, and seconds.
+-The clock should run by itself. (Hint: look at JavaScript setInterval function)
+
+
+*/
